2ac8be29de4a2f8423f9a02727012b3e74c337ef
Lucas Marchiori <sinonsrt@hotmail.com>
2022-04-22 13:39:33 -0300

    fix db connection

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import express from "express";
 
+import "reflect-metadata";
+import { AppDataSource } from "./data-source";
+
+AppDataSource.initialize();
+
 const app = express();
 
 app.listen(3333, () => console.log("Server already up 🚀"));
